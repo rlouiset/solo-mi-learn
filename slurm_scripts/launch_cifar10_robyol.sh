20173dfa9719ef61cc921febb09cdf03b584ed45
rlouiset <robin.louiset@gmail.com>
2024-12-27 15:06:11 +0100

    byol update

---
+++ slurm_scripts/launch_cifar10_robyol.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH --job-name robyol_CIFAR100
+#SBATCH --time=00-23:59:00
+#SBATCH --nodes=1
+#SBATCH --mem 80G
+#SBATCH --cpus-per-task=8
+#SBATCH --gres=gpu:1
+#SBATCH --gpus-per-node=1
+#SBATCH --partition A100
+#SBATCH --output robyol_CIFAR100.txt
+
+export PATH=/home/ids/rareme/miniconda3/bin:$PATH
+source activate base
+conda activate py39
+srun python3 main_pretrain.py \
+    # path to training script folder
+    --config-path scripts/pretrain/cifar/ \
+    # training config name
+    --config-name robyol.yaml
+    # add new arguments (e.g. those not defined in the yaml files)
+    # by doing ++new_argument=VALUE
+    # pytorch lightning's arguments can be added here as well.
