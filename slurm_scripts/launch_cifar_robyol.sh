b09fdca9af93dcd99a238ac44d50e7cef3598b4f
rlouiset <robin.louiset@gmail.com>
2024-12-28 13:12:01 +0100

    byol update

--- slurm_scripts/launch_cifar_robyol.sh
+++ slurm_scripts/launch_cifar_robyol.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name robyol_CIFAR10
+#SBATCH --job-name mocov3_CIFAR10
 #SBATCH --time=00-23:59:00
 #SBATCH --nodes=1
 #SBATCH --mem 80G
@@ -8,9 +8,9 @@
 #SBATCH --gres=gpu:1
 #SBATCH --gpus-per-node=1
 #SBATCH --partition V100
-#SBATCH --output robyol_CIFAR10.txt
+#SBATCH --output mocov3_CIFAR10.txt
 
 export PATH=/home/ids/rareme/miniconda3/bin:$PATH
 source activate base
 conda activate solo_learn
-srun python3 main_pretrain.py --config-path scripts/pretrain/cifar/ --config-name robyol.yaml
+srun python3 main_pretrain.py --config-path scripts/pretrain/cifar/ --config-name mocov3.yaml
