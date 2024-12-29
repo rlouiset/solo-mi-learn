3712c08a60f766d7068d24b470be17e5b9904e9d
rlouiset <robin.louiset@gmail.com>
2024-12-29 22:02:48 +0100

    byol update

--- solo/losses/barlow.py
+++ solo/losses/barlow.py
@@ -56,38 +56,7 @@ def barlow_loss_func(
     diag = torch.eye(D, device=corr.device)
     cdif = (corr - diag).pow(2)
     cdif[~diag.bool()] *= lamb
+    cdif[diag.bool()] *= 0
     loss = scale_loss * cdif.sum()
     return loss
 
-
-def barlow_loss_func_no_align(
-    z1: torch.Tensor, z2: torch.Tensor, lamb: float = 5e-3, scale_loss: float = 0.025
-) -> torch.Tensor:
-    """Computes Barlow Twins' loss given batch of projected features z1 from view 1 and
-    projected features z2 from view 2.
-
-    Args:
-        z1 (torch.Tensor): NxD Tensor containing projected features from view 1.
-        z2 (torch.Tensor): NxD Tensor containing projected features from view 2.
-        lamb (float, optional): off-diagonal scaling factor for the cross-covariance matrix.
-            Defaults to 5e-3.
-        scale_loss (float, optional): final scaling factor of the loss. Defaults to 0.025.
-
-    Returns:
-        torch.Tensor: Barlow Twins' loss.
-    """
-
-    N, D = z1.size()
-
-    # to match the original code
-    bn = torch.nn.BatchNorm1d(D, affine=False).to(z1.device)
-    z1 = bn(z1)
-    z2 = bn(z2)
-
-    # empirical cross-correlation matrix
-    c = self.bn(z1).T @ self.bn(z2)
-
-    # on_diag = torch.diagonal(c).add_(-1).pow_(2).sum()
-    off_diag = off_diagonal(c).pow_(2).sum()
-    loss = lambd * off_diag
-    return loss
