5ec8f5e1bb037fc189ed64b5b9c960b4bd406e66
rlouiset <robin.louiset@gmail.com>
2024-12-30 13:30:03 +0100

    byol update

--- slurm_scripts/launch_cifar_robyol.sh
+++ slurm_scripts/launch_cifar_robyol.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#SBATCH --job-name robyol_0.0001_au_CIFAR10
+#SBATCH --job-name robyol_0.0005_au_CIFAR10
 #SBATCH --time=00-23:59:00
 #SBATCH --nodes=1
 #SBATCH --mem 80G
 #SBATCH --cpus-per-task=8
 #SBATCH --gres=gpu:1
 #SBATCH --gpus-per-node=1
-#SBATCH --partition A100
+#SBATCH --partition V100
 #SBATCH --output robyol_0.0001_au_CIFAR10.txt
 
 export PATH=/home/ids/rareme/miniconda3/bin:$PATH
