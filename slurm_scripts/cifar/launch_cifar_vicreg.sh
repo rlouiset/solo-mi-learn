adce62b8b8df18805b06dd9fbf159d1e4fc7d48b
rlouiset <robin.louiset@gmail.com>
2025-01-12 11:27:18 +0100

    medmnist

--- slurm_scripts/cifar/launch_cifar_vicreg.sh
+++ slurm_scripts/cifar/launch_cifar_vicreg.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-#SBATCH --job-name barlow_bs_512_CIFAR100
+#SBATCH --job-name vicreg_bs_512_CIFAR10
 #SBATCH --time=00-23:59:00
 #SBATCH --nodes=1
-#SBATCH --mem 80G
+#SBATCH --mem 40G
 #SBATCH --cpus-per-task=8
 #SBATCH --gres=gpu:1
 #SBATCH --gpus-per-node=1
 #SBATCH --partition V100
-#SBATCH --output barlow_bs_512_CIFAR100.txt
+#SBATCH --output vicreg_bs_512_CIFAR10.txt
 
 export PATH=/home/ids/rareme/miniconda3/bin:$PATH
 source activate base
-conda activate solo_learn
-srun python3 main_pretrain.py --config-path scripts/pretrain/cifar/ --config-name barlow.yaml
+srun python3 main_pretrain.py --config-path scripts/pretrain/cifar/ --config-name vicreg.yaml
--- slurm_scripts/stl/launch_cifar_byol.sh
+++ slurm_scripts/stl/launch_cifar_byol.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-#SBATCH --job-name byol_bs_512_STL10
-#SBATCH --time=00-23:59:00
+#SBATCH --job-name ssl_bs_512_STL10
+#SBATCH --time=01-23:59:00
 #SBATCH --nodes=1
 #SBATCH --mem 50G
 #SBATCH --cpus-per-task=8
 #SBATCH --gres=gpu:1
 #SBATCH --gpus-per-node=1
 #SBATCH --partition A100
-#SBATCH --output byol_bs_512_STL10.txt
+#SBATCH --output ssl_bs_512_STL10.txt
 
 export PATH=/home/ids/rareme/miniconda3/bin:$PATH
 source activate base
-srun python3 main_pretrain.py --config-path scripts/pretrain/stl/ --config-name robyol.yaml
+srun python3 main_pretrain.py --config-path scripts/pretrain/stl/ --config-name simclr.yaml
+srun python3 main_pretrain.py --config-path scripts/pretrain/stl/ --config-name dino.yaml
+srun python3 main_pretrain.py --config-path scripts/pretrain/stl/ --config-name barlow.yaml
+
+
 
