72e7eb8a48136849b9d209d6eca2d16412202093
rlouiset <robin.louiset@gmail.com>
2025-01-17 22:49:19 +0100

    vicreg_s10

--- slurm_scripts/stl/launch_stl_xps.sh
+++ slurm_scripts/stl/launch_stl_xps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name simsiam_bs_512_STL10
+#SBATCH --job-name vicreg_bs_512_STL10
 #SBATCH --time=00-23:59:00
 #SBATCH --nodes=1
 #SBATCH --mem 50G
@@ -8,11 +8,11 @@
 #SBATCH --gres=gpu:1
 #SBATCH --gpus-per-node=1
 #SBATCH --partition V100
-#SBATCH --output simsiam_bs_512_STL10.txt
+#SBATCH --output vicreg_bs_512_STL10.txt
 
 export PATH=/home/ids/rareme/miniconda3/bin:$PATH
 source activate base
-srun python3 main_pretrain.py --config-path scripts/pretrain/stl/ --config-name simsiam.yaml
+srun python3 main_pretrain.py --config-path scripts/pretrain/stl/ --config-name vicreg.yaml
 
 
 
