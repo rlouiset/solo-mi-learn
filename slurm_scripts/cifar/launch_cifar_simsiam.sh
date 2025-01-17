b9388011c33e86805aea2434ed81dbbf4ab5f0b4
rlouiset <robin.louiset@gmail.com>
2025-01-17 15:46:15 +0100

    byol3steps

--- slurm_scripts/cifar/launch_cifar_simsiam.sh
+++ slurm_scripts/cifar/launch_cifar_simsiam.sh
@@ -7,8 +7,8 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --gres=gpu:1
 #SBATCH --gpus-per-node=1
-#SBATCH --partition A100
-#SBATCH --output simsiam_512_CIFAR10.txt
+#SBATCH --partition V100
+#SBATCH --output simsiam_512_CIFAR100.txt
 
 export PATH=/home/ids/rareme/miniconda3/bin:$PATH
 source activate base
