f52e4d076a9220f3e3cfebfef74da4e8a40d0ea7
rlouiset <robin.louiset@gmail.com>
2025-01-17 12:04:49 +0100

    byol3steps

--- slurm_scripts/cifar/launch_cifar_byol.sh
+++ slurm_scripts/cifar/launch_cifar_byol.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name byol3steps_bs_512_CIFAR100
+#SBATCH --job-name byol3steps_sch_bs_512_CIFAR100
 #SBATCH --time=00-23:59:00
 #SBATCH --nodes=1
 #SBATCH --mem 80G
@@ -8,7 +8,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --gpus-per-node=1
 #SBATCH --partition A100
-#SBATCH --output byol3steps_bs_512_CIFAR100.txt
+#SBATCH --output byol3steps_sch_bs_512_CIFAR100.txt
 
 export PATH=/home/ids/rareme/miniconda3/bin:$PATH
 source activate base
