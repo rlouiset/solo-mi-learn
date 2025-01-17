7f8158b2aa5107efbeecc4a84661c6552a124d78
rlouiset <robin.louiset@gmail.com>
2025-01-17 15:44:25 +0100

    byol3steps

---
+++ slurm_scripts/cifar/launch_cifar_simsiam.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --job-name simsiam_bs_512_CIFAR100
+#SBATCH --time=00-23:59:00
+#SBATCH --nodes=1
+#SBATCH --mem 80G
+#SBATCH --cpus-per-task=8
+#SBATCH --gres=gpu:1
+#SBATCH --gpus-per-node=1
+#SBATCH --partition A100
+#SBATCH --output simsiam_512_CIFAR10.txt
+
+export PATH=/home/ids/rareme/miniconda3/bin:$PATH
+source activate base
+srun python3 main_pretrain.py --config-path scripts/pretrain/cifar/ --config-name simsiam.yaml
