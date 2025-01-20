75abab86867518ad2f25ca35be68b68254dc1505
rlouiset <robin.louiset@gmail.com>
2025-01-20 13:58:40 +0100

    robyol-0-medmnist

--- slurm_scripts/med_mnist/launch_medmnist_robyol.sh
+++ slurm_scripts/med_mnist/launch_medmnist_robyol.sh
@@ -13,5 +13,8 @@
 export PATH=/home/ids/rareme/miniconda3/bin:$PATH
 source activate base
 srun python3 main_pretrain.py --config-path scripts/pretrain/med-mnist/ --config-name robyol_blood.yaml
+srun python3 main_pretrain.py --config-path scripts/pretrain/med-mnist/ --config-name robyol_blood.yaml
+srun python3 main_pretrain.py --config-path scripts/pretrain/med-mnist/ --config-name robyol_blood.yaml
+
 
 
