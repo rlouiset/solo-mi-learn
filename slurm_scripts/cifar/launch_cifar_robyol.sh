a7c7c4a97e2a589d8f503c8cd7730c7eeac82c80
rlouiset <robin.louiset@gmail.com>
2025-01-22 21:52:26 +0100

    robyol

--- slurm_scripts/cifar/launch_cifar_robyol.sh
+++ slurm_scripts/cifar/launch_cifar_robyol.sh
@@ -7,9 +7,9 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --gres=gpu:1
 #SBATCH --gpus-per-node=1
-#SBATCH --partition A100
+#SBATCH --partition V100
 #SBATCH --output robyol_0.001-unif_CIFAR100
 
 export PATH=/home/ids/rareme/miniconda3/bin:$PATH
 source activate base
-srun python3 main_pretrain.py --config-path scripts/pretrain/cifar/ --config-name robyol.yaml
+srun python3 main_pretrain.py --config-path scripts/pretrain/cifar/ --config-name byol3steps.yaml
--- solo/methods/base.py
+++ solo/methods/base.py
@@ -836,7 +836,7 @@ class BaseMomentumMethod(BaseMethod):
             batch_idx (int): index of the batch.
         """
 
-        if self.trainer.global_step > self.last_step:
+        if self.trainer.global_step > self.last_step+1:
             # update momentum backbone and projector
             momentum_pairs = self.momentum_pairs
             for mp in momentum_pairs:
@@ -848,7 +848,8 @@ class BaseMomentumMethod(BaseMethod):
                 cur_step=self.trainer.global_step,
                 max_steps=self.trainer.estimated_stepping_batches,
             )
-        self.last_step = self.trainer.global_step
+            self.last_step = self.trainer.global_step
+        self.last_step = self.last_step + 1
 
     def validation_step(
         self,
