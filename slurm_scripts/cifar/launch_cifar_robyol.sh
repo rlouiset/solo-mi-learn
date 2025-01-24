951de6a9e9a3dca9ce29d59ce6e0d4da58a75b8b
rlouiset <robin.louiset@gmail.com>
2025-01-24 10:17:59 +0100

    robyol_cifar

--- slurm_scripts/cifar/launch_cifar_robyol.sh
+++ slurm_scripts/cifar/launch_cifar_robyol.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#SBATCH --job-name robyol_0.00001-unif_CIFAR10
+#SBATCH --job-name robyol_0.00001-align-unif_CIFAR10et100
 #SBATCH --time=00-23:59:00
 #SBATCH --nodes=1
 #SBATCH --mem 80G
 #SBATCH --cpus-per-task=8
 #SBATCH --gres=gpu:1
 #SBATCH --gpus-per-node=1
-#SBATCH --partition V100
-#SBATCH --output robyol_0.00001-unif_CIFAR10.txt
+#SBATCH --partition A100
+#SBATCH --output robyol_0.00001-align-unif_CIFAR10et100.txt
 
 export PATH=/home/ids/rareme/miniconda3/bin:$PATH
 source activate base
