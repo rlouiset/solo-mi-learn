aaa7166e65f1c20769e7feaf1776d6a297947bff
rlouiset <robin.louiset@gmail.com>
2025-01-25 10:46:06 +0100

    robyol-stl

--- slurm_scripts/stl/launch_stl_xps.sh
+++ slurm_scripts/stl/launch_stl_xps.sh
@@ -13,7 +13,6 @@
 export PATH=/home/ids/rareme/miniconda3/bin:$PATH
 source activate base
 srun python3 main_pretrain.py --config-path scripts/pretrain/stl/ --config-name robyol.yaml
-srun python3 main_pretrain.py --config-path scripts/pretrain/stl/ --config-name robyol_2.yaml
 
 
 
