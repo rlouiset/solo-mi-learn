236649a79899eaf358983e532ec724cfdb80f383
rlouiset <robin.louiset@gmail.com>
2025-01-26 11:21:15 +0100

    robyol-cifar-mc

--- slurm_scripts/cifar/launch_cifar_byol.sh
+++ slurm_scripts/cifar/launch_cifar_byol.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name byol-mc_CIFAR10
+#SBATCH --job-name robyol-mc_CIFAR10
 #SBATCH --time=00-23:59:00
 #SBATCH --nodes=1
 #SBATCH --mem 80G
@@ -8,8 +8,8 @@
 #SBATCH --gres=gpu:1
 #SBATCH --gpus-per-node=1
 #SBATCH --partition A100
-#SBATCH --output byol-mc-lse_CIFAR10.txt
+#SBATCH --output robyol-mc_CIFAR10.txt
 
 export PATH=/home/ids/rareme/miniconda3/bin:$PATH
 source activate base
-srun python3 main_pretrain.py --config-path scripts/pretrain/cifar-multicrop/ --config-name byol.yaml
+srun python3 main_pretrain.py --config-path scripts/pretrain/cifar-multicrop/ --config-name robyol.yaml
