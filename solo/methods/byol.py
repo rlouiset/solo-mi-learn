5f36c5023483effc7353d1cccfaa2b6f88dd02d0
rlouiset <robin.louiset@gmail.com>
2025-01-26 12:24:25 +0100

    robyol-cifar-mc

--- solo/methods/byol.py
+++ solo/methods/byol.py
@@ -184,19 +184,19 @@ class BYOL(BaseMomentumMethod):
         Z_momentum = out["momentum_z"]
 
         # ------- negative cosine similarity loss -------
-        """neg_cos_sim = 0
+        neg_cos_sim = 0
         for v1 in range(self.num_large_crops):
             for v2 in np.delete(range(self.num_crops), v1):
-                neg_cos_sim += byol_loss_func(P[v2], Z_momentum[v1])"""
+                neg_cos_sim += byol_loss_func(P[v2], Z_momentum[v1])
 
-        neg_cos_sim = 0
+        """neg_cos_sim = 0
         for v1 in range(self.num_large_crops):
             neg_cos_sim_i = []
             for v2 in np.delete(range(self.num_crops), v1):
                 neg_cos_sim_i.append((- 2 + 2 * (F.normalize(P[v2], dim=-1) * F.normalize(Z_momentum[v1].detach(), dim=-1)).sum(dim=-1)).exp())
             neg_cos_sim_i = sum(neg_cos_sim_i) / (self.num_crops - 1)
             neg_cos_sim += neg_cos_sim_i.log().mean()
-        neg_cos_sim = - neg_cos_sim
+        neg_cos_sim = - neg_cos_sim"""
 
         # calculate std of features
         with torch.no_grad():
