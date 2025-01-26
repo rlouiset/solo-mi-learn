394dc1e55c7d90fb921659e9b218354355e1e8e4
rlouiset <robin.louiset@gmail.com>
2025-01-26 11:27:25 +0100

    rosiam-cifar

--- slurm_scripts/cifar/launch_cifar_simsiam.sh
+++ slurm_scripts/cifar/launch_cifar_simsiam.sh
@@ -13,3 +13,5 @@
 export PATH=/home/ids/rareme/miniconda3/bin:$PATH
 source activate base
 srun python3 main_pretrain.py --config-path scripts/pretrain/cifar/ --config-name simsiam.yaml
+srun python3 main_pretrain.py --config-path scripts/pretrain/cifar/ --config-name simsiam_2.yaml
+
