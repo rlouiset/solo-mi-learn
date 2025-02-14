0418c6b375cd10905b71a5dca531f0fc9dbb66fc
rlouiset <robin.louiset@gmail.com>
2025-02-14 14:58:08 +0100

    robyol-mc

--- solo/methods/robyol.py
+++ solo/methods/robyol.py
@@ -205,15 +205,13 @@ class RoBYOL(BaseMomentumMethod):
         for v1 in range(self.num_large_crops):
             for v2 in np.delete(range(self.num_crops), v1):
                 neg_cos_sim += byol_loss_func(P[v2], Z_momentum[v1])
-        neg_cos_sim /= (self.num_crops-1)
-
 
         # ------- negative cosine similarity loss -------
         au_loss = 0
         for v1 in range(self.num_large_crops):
-            for v2 in np.delete(range(self.num_crops), v1):
+            for v2 in np.delete(range(self.num_large_crops), v1):
                 au_loss += uniform_loss_func(F.normalize(Z[v1], dim=-1))
-                au_loss += align_loss_func(F.normalize(Z[v1], dim=-1), F.normalize(Z[v2], dim=-1)) / (self.num_crops - 1)
+                au_loss += align_loss_func(F.normalize(Z[v1], dim=-1), F.normalize(Z[v2], dim=-1))
 
         # calculate std of features
         with torch.no_grad():
