e619d5cf739cf836579020c61c22b4617f21d28f
rlouiset <robin.louiset@gmail.com>
2025-02-15 16:13:38 +0100

    robyol-mc

--- solo/download_in1k.py
+++ solo/download_in1k.py
@@ -1,6 +1,6 @@
 from datasets import load_dataset
 
 # If the dataset is gated/private, make sure you have run huggingface-cli login
-ds = load_dataset("clane9/imagenet-100", data_dir="datasets/in100")
+ds = load_dataset("clane9/imagenet-100")
 
 print(ds)
