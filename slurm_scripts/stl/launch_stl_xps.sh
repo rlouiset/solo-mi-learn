650b0bdab598a14810615f04b0b1abbdf2e8c7d3
rlouiset <robin.louiset@gmail.com>
2025-02-16 15:24:53 +0100

    robyol-mc

--- slurm_scripts/stl/launch_stl_xps.sh
+++ slurm_scripts/stl/launch_stl_xps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name robyol_bs_512_STL10
+#SBATCH --job-name robyol-mc_bs_512_STL10
 #SBATCH --time=00-23:59:00
 #SBATCH --nodes=1
 #SBATCH --mem 50G
@@ -8,7 +8,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --gpus-per-node=1
 #SBATCH --partition V100
-#SBATCH --output robyol_bs_512_STL10.txt
+#SBATCH --output robyol-mc_bs_512_STL10.txt
 
 export PATH=/home/ids/rareme/miniconda3/bin:$PATH
 source activate base
