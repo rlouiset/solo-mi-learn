9704f622a61a3efe45e97fbf884b1201590f5947
rlouiset <robin.louiset@gmail.com>
2025-02-19 23:13:09 +0100

    cfr100

--- slurm_scripts/cifar/launch_cifar_byol.sh
+++ slurm_scripts/cifar/launch_cifar_byol.sh
@@ -12,4 +12,4 @@
 
 export PATH=/home/ids/rareme/miniconda3/bin:$PATH
 source activate base
-srun python3 main_pretrain.py --config-path scripts/pretrain/cifar-multicrop/ --config-name robyol.yaml
+srun python3 main_pretrain.py --config-path scripts/pretrain/cifar-multicrop/ --config-name byol.yaml
