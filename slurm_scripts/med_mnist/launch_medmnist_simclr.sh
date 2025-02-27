ea8226fdef45e072aaab84cc62171af3d70cc46c
rlouiset <robin.louiset@gmail.com>
2025-02-27 22:56:47 +0100

    blood

--- slurm_scripts/med_mnist/launch_medmnist_simclr.sh
+++ slurm_scripts/med_mnist/launch_medmnist_simclr.sh
@@ -5,9 +5,9 @@
 #SBATCH --nodes=1
 #SBATCH --mem 80G
 #SBATCH --cpus-per-task=4
-#SBATCH --gres=gpu:2
-#SBATCH --gpus-per-node=2
-#SBATCH --partition V100
+#SBATCH --gres=gpu:1
+#SBATCH --gpus-per-node=1
+#SBATCH --partition A100
 #SBATCH --output simclr_bs_512_bloodmnist.txt
 
 export PATH=/home/ids/rareme/miniconda3/bin:$PATH
