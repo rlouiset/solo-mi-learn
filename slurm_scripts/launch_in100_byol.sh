8bc7547273cd7c9666b62c76a0d7e443adf27327
rlouiset <robin.louiset@gmail.com>
2025-02-27 10:16:19 +0100

    in100

--- slurm_scripts/launch_in100_byol.sh
+++ slurm_scripts/launch_in100_byol.sh
@@ -3,11 +3,11 @@
 #SBATCH --job-name byol_bs_512_IN100
 #SBATCH --time=00-23:59:00
 #SBATCH --nodes=1
-#SBATCH --mem 100G
+#SBATCH --mem 80G
 #SBATCH --ntasks-per-node=2
 #SBATCH --cpus-per-task=4
 #SBATCH --gpus-per-node=2
-#SBATCH --gres=gpu:4
+#SBATCH --gres=gpu:2
 #SBATCH --nodes=2
 #SBATCH --partition V100
 #SBATCH --output byol_bs_512_IN100.txt
