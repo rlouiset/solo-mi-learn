127faf4e37926e597653e9459e337e2ae99f94fb
rlouiset <robin.louiset@gmail.com>
2025-02-28 11:16:47 +0100

    blood

--- solo/data/classification_dataloader.py
+++ solo/data/classification_dataloader.py
@@ -259,7 +259,7 @@ def prepare_datasets(
         )
         for i in range(len(train_dataset)):
             train_dataset[i][1][0] = train_dataset[i][1][0].astype(np.float16)
-            train_dataset[i][1][1] = train_dataset[i][1][1].astype(np.float16)
+
         DatasetClass = vars(medmnist)[dataset]
         val_dataset = DatasetClass(
             root=val_data_path,
@@ -270,7 +270,6 @@ def prepare_datasets(
         )
         for i in range(len(val_dataset)):
             val_dataset[i][1][0] = val_dataset[i][1][0].astype(np.float16)
-            val_dataset[i][1][1] = val_dataset[i][1][1].astype(np.float16)
 
     elif dataset == "stl10":
         train_dataset = STL10(
