a09f67ba35621a568792c341d2dd9aec09571476
rlouiset <robin.louiset@gmail.com>
2025-02-28 16:15:16 +0100

    blood

--- slurm_scripts/med_mnist/launch_medmnist_robyol.sh
+++ slurm_scripts/med_mnist/launch_medmnist_robyol.sh
@@ -5,14 +5,15 @@
 #SBATCH --nodes=1
 #SBATCH --mem 80G
 #SBATCH --cpus-per-task=4
-#SBATCH --gres=gpu:3
-#SBATCH --gpus-per-node=3
-#SBATCH --partition V100
+#SBATCH --gres=gpu:2
+#SBATCH --gpus-per-node=2
+#SBATCH --partition A100
 #SBATCH --output robyol_bs_512_bloodmnist.txt
 
 export PATH=/home/ids/rareme/miniconda3/bin:$PATH
 source activate base
 srun python3 main_pretrain.py --config-path scripts/pretrain/med-mnist/ --config-name robyol_blood.yaml
+srun python3 main_pretrain.py --config-path scripts/pretrain/med-mnist/ --config-name byol_blood.yaml
 
 
 
