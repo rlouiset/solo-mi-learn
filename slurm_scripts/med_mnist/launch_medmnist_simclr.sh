d6a752d6a3122953f8d64ebbd709813796c0f9d1
rlouiset <robin.louiset@gmail.com>
2025-02-28 11:53:25 +0100

    blood

--- slurm_scripts/med_mnist/launch_medmnist_simclr.sh
+++ slurm_scripts/med_mnist/launch_medmnist_simclr.sh
@@ -3,14 +3,13 @@
 #SBATCH --job-name simclr_bs_512_bloodmnist
 #SBATCH --time=00-23:59:00
 #SBATCH --nodes=1
-#SBATCH --mem 80G
-#SBATCH --cpus-per-task=4
-#SBATCH --gres=gpu:1
-#SBATCH --gpus-per-node=1
+#SBATCH --mem 100G
+#SBATCH --cpus-per-task=8
+#SBATCH --gres=gpu:2
+#SBATCH --gpus-per-node=2
 #SBATCH --partition V100
 #SBATCH --output simclr_bs_512_bloodmnist.txt
 
 export PATH=/home/ids/rareme/miniconda3/bin:$PATH
 source activate base
-conda activate solo_learn
 srun python3 main_pretrain.py --config-path scripts/pretrain/med-mnist/ --config-name simclr_blood.yaml
