d5cd140d1c7487c59bdf7641e204ae9387554f23
rlouiset <robin.louiset@gmail.com>
2025-02-28 18:10:55 +0100

    blood

--- slurm_scripts/med_mnist/launch_medmnist_vicreg.sh
+++ slurm_scripts/med_mnist/launch_medmnist_vicreg.sh
@@ -12,7 +12,7 @@
 
 export PATH=/home/ids/rareme/miniconda3/bin:$PATH
 source activate base
-srun python3 main_pretrain.py --config-path scripts/pretrain/med-mnist/ --config-name barlow_blood.yaml
+srun python3 main_pretrain.py --config-path scripts/pretrain/med-mnist/ --config-name vicreg_blood.yaml
 
 
 
