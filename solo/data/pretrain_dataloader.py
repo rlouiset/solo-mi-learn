90c77e188cb13d74bb6eded9b8dd992f13f57ac9
rlouiset <robin.louiset@gmail.com>
2025-02-28 11:02:35 +0100

    blood

--- solo/data/pretrain_dataloader.py
+++ solo/data/pretrain_dataloader.py
@@ -339,6 +339,15 @@ def prepare_datasets(
             transform=transform,
             size=128
         )
+        print(len(train_dataset[0][1]))
+        """for i in range(len(train_dataset[0][1])):
+            train_dataset[0][1][i] = train_dataset[0][1][i].to(dtype=torch.float16)"""
+        print(train_dataset[0][0])
+        print(train_dataset[0][1][0].shape)
+        print(train_dataset[0][1][0][0].shape)
+        print(type(train_dataset[0][1][0]))
+        print(train_dataset[0][1][0])
+        print(debug)
 
     elif dataset == "stl10":
         train_dataset = dataset_with_index(STL10)(
