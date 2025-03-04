d6e20e86ccff4702c838c8bd4ec35c4fd67a4a13
rlouiset <robin.louiset@gmail.com>
2025-03-04 23:29:19 +0100

    in

--- slurm_scripts/launch_in100_byol.sh
+++ slurm_scripts/launch_in100_byol.sh
@@ -5,11 +5,11 @@
 #SBATCH --nodes=1
 #SBATCH --mem 120G
 #SBATCH --cpus-per-task=10
-#SBATCH --gres=gpu:3
+#SBATCH --gres=gpu:2
 #SBATCH --partition A100
 #SBATCH --output byol_bs_512_A100_IN100.txt
 
 export PATH=/home/ids/rareme/miniconda3/bin:$PATH
 source activate base
 nvidia-smi
-srun python3 main_pretrain.py --config-path scripts/pretrain/imagenet-100-multicrop/ --config-name robyol.yaml
+srun python3 main_pretrain.py --config-path scripts/pretrain/imagenet-100/ --config-name byol.yaml
