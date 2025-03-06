4f4c00245c80799b601021271e9c60158dbaeb08
rlouiset <robin.louiset@gmail.com>
2025-03-06 22:03:09 +0100

    in

--- slurm_scripts/launch_in100_byol.sh
+++ slurm_scripts/launch_in100_byol.sh
@@ -6,7 +6,7 @@
 #SBATCH --mem 120G
 #SBATCH --cpus-per-task=10
 #SBATCH --gres=gpu:3
-#SBATCH --partition A100
+#SBATCH --partition L40S
 #SBATCH --output byol_bs_512_A100_IN100.txt
 
 export PATH=/home/ids/rareme/miniconda3/bin:$PATH
