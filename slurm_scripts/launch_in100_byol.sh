2dd73879bb431576d35b28da5ddf14a1b876c0b5
rlouiset <robin.louiset@gmail.com>
2025-03-07 21:55:24 +0100

    in

--- slurm_scripts/launch_in100_byol.sh
+++ slurm_scripts/launch_in100_byol.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#SBATCH --job-name dino_bs_512_A100_IN100
+#SBATCH --job-name simsiam_bs_512_A100_IN100
 #SBATCH --time=00-23:59:00
 #SBATCH --nodes=1
 #SBATCH --mem 120G
 #SBATCH --cpus-per-task=10
 #SBATCH --gres=gpu:2
-#SBATCH --partition L40S
-#SBATCH --output dino_bs_512_A100_IN100.txt
+#SBATCH --partition A100
+#SBATCH --output simsiam_bs_512_A100_IN100.txt
 
 export PATH=/home/ids/rareme/miniconda3/bin:$PATH
 source activate base
