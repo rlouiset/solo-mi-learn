057daf59b9cde4214be95fd79a97432c763010a1
rlouiset <robin.louiset@gmail.com>
2025-03-07 16:00:53 +0100

    in

--- slurm_scripts/launch_in100_byol.sh
+++ slurm_scripts/launch_in100_byol.sh
@@ -6,10 +6,10 @@
 #SBATCH --mem 120G
 #SBATCH --cpus-per-task=10
 #SBATCH --gres=gpu:2
-#SBATCH --partition L40S
+#SBATCH --partition A100
 #SBATCH --output dino_bs_512_A100_IN100.txt
 
 export PATH=/home/ids/rareme/miniconda3/bin:$PATH
 source activate base
 nvidia-smi
-srun python3 main_pretrain.py --config-path scripts/pretrain/imagenet-100/ --config-name dino_vit.yaml
+srun python3 main_pretrain.py --config-path scripts/pretrain/imagenet-100/ --config-name simsiam_vit.yaml
