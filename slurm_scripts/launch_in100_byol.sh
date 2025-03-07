c45a42d55b494183efc741b44fc3b5fce1864bdd
rlouiset <robin.louiset@gmail.com>
2025-03-07 15:58:48 +0100

    in

--- slurm_scripts/launch_in100_byol.sh
+++ slurm_scripts/launch_in100_byol.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#SBATCH --job-name byol_bs_512_A100_IN100
+#SBATCH --job-name dino_bs_512_A100_IN100
 #SBATCH --time=00-23:59:00
 #SBATCH --nodes=1
 #SBATCH --mem 120G
 #SBATCH --cpus-per-task=10
-#SBATCH --gres=gpu:3
-#SBATCH --partition A100
+#SBATCH --gres=gpu:2
+#SBATCH --partition L40S
 #SBATCH --output simsiam_bs_512_A100_IN100.txt
 
 export PATH=/home/ids/rareme/miniconda3/bin:$PATH
 source activate base
 nvidia-smi
-srun python3 main_pretrain.py --config-path scripts/pretrain/imagenet-100-multicrop/ --config-name robyol.yaml
+srun python3 main_pretrain.py --config-path scripts/pretrain/imagenet-100-multicrop/ --config-name simsiam_vit.yaml
