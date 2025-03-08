418a60cd21eecb0e500ba2bebd25e4c5918330c4
rlouiset <robin.louiset@gmail.com>
2025-03-08 08:05:37 +0100

    in

--- slurm_scripts/launch_in100_byol.sh
+++ slurm_scripts/launch_in100_byol.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#SBATCH --job-name simsiam_bs_512_A100_IN100
+#SBATCH --job-name byol_bs_512_A100_IN100
 #SBATCH --time=00-23:59:00
 #SBATCH --nodes=1
 #SBATCH --mem 120G
 #SBATCH --cpus-per-task=10
 #SBATCH --gres=gpu:3
 #SBATCH --partition A100
-#SBATCH --output simsiam_bs_512_A100_IN100.txt
+#SBATCH --output robyol-mc_bs_512_A100_IN100.txt
 
 export PATH=/home/ids/rareme/miniconda3/bin:$PATH
 source activate base
