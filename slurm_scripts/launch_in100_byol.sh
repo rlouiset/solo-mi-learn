1a86c5e22d4b581a74f5bdfae89293cf8c7c446f
rlouiset <robin.louiset@gmail.com>
2025-04-24 09:21:12 +0200

    in

--- slurm_scripts/launch_in100_byol.sh
+++ slurm_scripts/launch_in100_byol.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#SBATCH --job-name byol_bs_512_A100_IN100
+#SBATCH --job-name rosiam_bs_512_A100_IN100
 #SBATCH --time=00-23:59:00
 #SBATCH --nodes=1
-#SBATCH --mem 120G
-#SBATCH --cpus-per-task=10
+#SBATCH --mem 100G
+#SBATCH --cpus-per-task=8
 #SBATCH --gres=gpu:3
-#SBATCH --partition L40S
-#SBATCH --output simsiam_bs_512_A100_IN100.txt
+#SBATCH --partition A100
+#SBATCH --output rosiam_bs_512_A100_IN100.txt
 
 export PATH=/home/ids/rareme/miniconda3/bin:$PATH
 source activate base
 nvidia-smi
-srun python3 main_pretrain.py --config-path scripts/pretrain/imagenet-100-multicrop/ --config-name simsiam_vit.yaml
+srun python3 main_pretrain.py --config-path scripts/pretrain/imagenet-100/ --config-name simsiam_vit.yaml
