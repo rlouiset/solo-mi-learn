b2648eb38fa7cb9ebe349bad76693ac88f805b4c
rlouiset <robin.louiset@gmail.com>
2025-05-01 12:57:34 +0200

    in

--- slurm_scripts/launch_in100_byol.sh
+++ slurm_scripts/launch_in100_byol.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#SBATCH --job-name vicreg_bs_256_A100_IN100
+#SBATCH --job-name barlow_bs_256_A100_IN100
 #SBATCH --time=00-23:59:00
 #SBATCH --nodes=1
 #SBATCH --mem 100G
 #SBATCH --cpus-per-task=8
 #SBATCH --gres=gpu:2
 #SBATCH --partition A100
-#SBATCH --output vicreg_bs_256_A100_IN100.txt
+#SBATCH --output barlow_bs_256_A100_IN100.txt
 
 export PATH=/home/ids/rareme/miniconda3/bin:$PATH
 source activate base
 nvidia-smi
-srun python3 main_pretrain.py --config-path scripts/pretrain/imagenet-100/ --config-name vicreg_vit.yaml
+srun python3 main_pretrain.py --config-path scripts/pretrain/imagenet-100/ --config-name barlow_vit.yaml
