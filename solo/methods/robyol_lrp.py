6f9580442dfc1b66f0cf52c51a48b4b93bfd7734
Robin Louiset <robinlouiset@MacBook-Pro-de-Robin.local>
2025-06-06 19:07:42 +0200

    robyol_lrp

--- solo/methods/robyol_lrp.py
+++ solo/methods/robyol_lrp.py
@@ -208,8 +208,9 @@ class RoBYOLLRP(BaseMomentumMethod):
         with torch.no_grad():
             for v1 in range(self.num_large_crops):
                 for v2 in np.delete(range(self.num_crops), v1):
-                    P = lrp(Z[v2].float(), Z_momentum[v1].float())
+                    P = lrp(F.normalize(Z[v2].float(), dim=-1), F.normalize(Z_momentum[v1].float(), dim=-1))
                     self.predictor.weight.mul_(0.9).add_(0.1 * P)
+                    self.predictor.weight = F.normalize(self.predictor.weight, dim=-1)
 
         neg_cos_sim = 0
         for v1 in range(self.num_large_crops):
