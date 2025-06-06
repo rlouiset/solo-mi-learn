7b40641561f57a61e12276c30c7148721ba88499
Robin Louiset <robinlouiset@MacBook-Pro-de-Robin.local>
2025-06-06 16:58:50 +0200

    robyol_lrp

--- solo/methods/robyol_lrp.py
+++ solo/methods/robyol_lrp.py
@@ -202,12 +202,9 @@ class RoBYOLLRP(BaseMomentumMethod):
         neg_cos_sim = 0
         for v1 in range(self.num_large_crops):
             for v2 in np.delete(range(self.num_crops), v1):
-                P = ne_predictor(F.normalize(Z[v2], dim=-1), F.normalize(Z_momentum[v1], dim=-1))
-                print(P.shape)
-                print(Z[v2].shape)
-                print(Z_momentum[v1].shape)
-                P = self.momentum_updater.cur_tau * P + (1-self.momentum_updater.cur_tau) * F.normalize(Z[v2], dim=-1)
-                neg_cos_sim += byol_loss_func(F.normalize(P, dim=-1), Z_momentum[v1])
+                P = F.normalize(ne_predictor(F.normalize(Z[v2], dim=-1), F.normalize(Z_momentum[v1], dim=-1)), dim=-1)
+                predictions = self.momentum_updater.cur_tau*(F.normalize(Z[v2], dim=-1)@P) + (1-self.momentum_updater.cur_tau) * F.normalize(Z[v2], dim=-1)
+                neg_cos_sim += byol_loss_func(predictions, Z_momentum[v1])
 
         # ------- negative cosine similarity loss -------
         au_loss = 0
