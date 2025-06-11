13bec44776db5c58bfacd410e3d3295078d639aa
Robin Louiset <robinlouiset@MacBook-Pro-de-Robin.local>
2025-06-11 10:56:28 +0200

    robyol_lrp

--- solo/methods/robyol_lrp.py
+++ solo/methods/robyol_lrp.py
@@ -234,7 +234,7 @@ class RoBYOLLRP(BaseMomentumMethod):
         neg_cos_sim = 0
         for v1 in range(self.num_large_crops):
             for v2 in np.delete(range(self.num_crops), v1):
-                EMA_P_v2 = self.momentum_updater.current_tau * P[v2] + (1-self.momentum_updater.current_tau) * Z[v2]
+                EMA_P_v2 = self.momentum_updater.cur_tau * P[v2] + (1-self.momentum_updater.cur_tau) * Z[v2]
                 neg_cos_sim += byol_loss_func(EMA_P_v2, Z_momentum[v1])
 
         self.manual_backward(neg_cos_sim + class_loss)
