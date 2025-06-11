e5f3ec2da759e10223b5ac47c83ef9f4f77cecb9
Robin Louiset <robinlouiset@MacBook-Pro-de-Robin.local>
2025-06-11 11:41:43 +0200

    robyol_lrp

--- main_pretrain.py
+++ main_pretrain.py
@@ -222,7 +222,7 @@ def main(cfg: DictConfig):
             "logger": wandb_logger if cfg.wandb.enabled else None,
             "callbacks": callbacks,
             "enable_checkpointing": False,
-            "strategy": DDPStrategy(find_unused_parameters=False)
+            "strategy": DDPStrategy(find_unused_parameters=True)
             if cfg.strategy == "ddp"
             else cfg.strategy,
         }
