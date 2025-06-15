defb565accadb9426c2fd2326ac16ec2cc8b4ffe
Robin Louiset <robinlouiset@MacBook-Pro-de-Robin.local>
2025-06-15 19:20:17 +0200

    robyol_lrp

--- slurm_scripts/stl/launch_stl_xps.sh
+++ slurm_scripts/stl/launch_stl_xps.sh
@@ -5,14 +5,13 @@
 #SBATCH --nodes=1
 #SBATCH --mem 50G
 #SBATCH --cpus-per-task=8
-#SBATCH --gres=gpu:1
-#SBATCH --gpus-per-node=1
+#SBATCH --gres=gpu:2
 #SBATCH --partition V100
 #SBATCH --output robyol-mc_bs_512_STL10.txt
 
 export PATH=/home/ids/rareme/miniconda3/bin:$PATH
 source activate base
-srun python3 main_pretrain.py --config-path scripts/pretrain/stl-multicrop/ --config-name robyol.yaml
+srun python3 main_pretrain.py --config-path scripts/pretrain/stl-multicrop/ --config-name robyol_lrp.yaml
 
 
 
