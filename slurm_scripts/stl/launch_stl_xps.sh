3be72f42f57a286abe47d1a9e9ffd78ee9578c64
Robin Louiset <robinlouiset@MacBook-Pro-de-Robin.local>
2025-06-16 11:47:08 +0200

    test stl

--- slurm_scripts/stl/launch_stl_xps.sh
+++ slurm_scripts/stl/launch_stl_xps.sh
@@ -11,7 +11,7 @@
 
 export PATH=/home/ids/rareme/miniconda3/bin:$PATH
 source activate base
-srun python3 main_pretrain.py --config-path scripts/pretrain/stl-multicrop/ --config-name robyol.yaml
+srun python3 main_pretrain.py --config-path scripts/pretrain/stl/ --config-name robyol.yaml
 
 
 
