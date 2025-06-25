7cfff6aed1353b3cc5d89da7cc0c3d45b865b3d5
Robin Louiset <robinlouiset@MacBook-Pro-de-Robin.local>
2025-06-25 11:07:10 +0200

    bs1024

--- slurm_scripts/stl/launch_stl_xps.sh
+++ slurm_scripts/stl/launch_stl_xps.sh
@@ -12,7 +12,7 @@
 export PATH=/home/ids/rareme/miniconda3/bin:$PATH
 source activate base
 srun python3 main_pretrain.py --config-path scripts/pretrain/stl/ --config-name simsiam.yaml
-srun python3 main_pretrain.py --config-path scripts/pretrain/stl/ --config-name dino.yaml
+srun python3 main_pretrain.py --config-path scripts/pretrain/stl/ --config-name barlow.yaml
 
 
 
