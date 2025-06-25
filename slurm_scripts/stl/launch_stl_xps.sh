1828153068033dd7ffe5412909a14cdc78819c2a
Robin Louiset <robinlouiset@mbp-de-robin.home>
2025-06-25 19:20:53 +0200

    bs1024

--- slurm_scripts/stl/launch_stl_xps.sh
+++ slurm_scripts/stl/launch_stl_xps.sh
@@ -5,14 +5,13 @@
 #SBATCH --nodes=1
 #SBATCH --mem 50G
 #SBATCH --cpus-per-task=8
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:2
 #SBATCH --partition A100
 #SBATCH --output robyol_bs_1024_STL10.txt
 
 export PATH=/home/ids/rareme/miniconda3/bin:$PATH
 source activate base
-srun python3 main_pretrain.py --config-path scripts/pretrain/stl/ --config-name simsiam.yaml
-srun python3 main_pretrain.py --config-path scripts/pretrain/stl/ --config-name barlow.yaml
+srun python3 main_pretrain.py --config-path scripts/pretrain/stl/ --config-name robyol.yaml
 
 
 
