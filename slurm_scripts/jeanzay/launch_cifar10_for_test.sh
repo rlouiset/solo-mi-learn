c88023047d1e09ee19980facd1bc8491b56573af
Robin Louiset <robinlouiset@MacBook-Pro-de-Robin.local>
2025-07-17 14:34:21 +0200

    jz

--- slurm_scripts/jeanzay/launch_cifar10_for_test.sh
+++ slurm_scripts/jeanzay/launch_cifar10_for_test.sh
@@ -8,8 +8,8 @@
 #SBATCH --gres=gpu:1
 #SBATCH --gpus-per-node=1
 #SBATCH --qos=qos_gpu-dev # QoS
-#SBATCH --constraint A100
-#SBATCH --account uik24xv
+#SBATCH --constraint a100
+#SBATCH --account uik24xv@a100
 #SBATCH --output byol.txt
 
 module purge # purge modules inherited by default
