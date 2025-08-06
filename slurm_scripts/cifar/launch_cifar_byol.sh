4f1dfacb39847e2625da52bf7b65b4bb3f20d333
Robin Louiset <robinlouiset@MacBook-Pro-de-Robin.local>
2025-08-06 10:39:10 +0200

    tracking residual entropy

--- slurm_scripts/cifar/launch_cifar_byol.sh
+++ slurm_scripts/cifar/launch_cifar_byol.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#SBATCH --job-name byol-mc
+#SBATCH --job-name byol
 #SBATCH --time=00-23:59:00
 #SBATCH --nodes=1
 #SBATCH --mem 80G
 #SBATCH --cpus-per-task=8
 #SBATCH --gres=gpu:1
 #SBATCH --gpus-per-node=1
-#SBATCH --partition A100
-#SBATCH --output byol-mc.txt
+#SBATCH --partition V100
+#SBATCH --output byol.txt
 
 export PATH=/home/ids/rareme/miniconda3/bin:$PATH
 source activate base
