689c7433ec20a74095759b31e8ac10cc74e94881
Robin Louiset <robinlouiset@MacBook-Pro-de-Robin.local>
2025-08-13 23:11:59 +0200

    robyol 4 layers 400 epochs

--- solo/methods/robyol.py
+++ solo/methods/robyol.py
@@ -73,6 +73,9 @@ class RoBYOL(BaseMomentumMethod):
             nn.Linear(pred_hidden_dim, pred_hidden_dim),
             nn.BatchNorm1d(pred_hidden_dim),
             nn.ReLU(),
+            nn.Linear(pred_hidden_dim, pred_hidden_dim),
+            nn.BatchNorm1d(pred_hidden_dim),
+            nn.ReLU(),
             nn.Linear(pred_hidden_dim, proj_output_dim),
         )
 
