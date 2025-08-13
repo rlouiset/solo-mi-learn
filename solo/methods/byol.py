bccaf03156ca4c5f6275686f3481e171f8462e0d
Robin Louiset <robinlouiset@MacBook-Pro-de-Robin.local>
2025-08-13 23:14:01 +0200

    robyol 3 layers 400 epochs

--- solo/methods/byol.py
+++ solo/methods/byol.py
@@ -72,12 +72,6 @@ class BYOL(BaseMomentumMethod):
             nn.Linear(pred_hidden_dim, pred_hidden_dim),
             nn.BatchNorm1d(pred_hidden_dim),
             nn.ReLU(),
-            nn.Linear(pred_hidden_dim, pred_hidden_dim),
-            nn.BatchNorm1d(pred_hidden_dim),
-            nn.ReLU(),
-            nn.Linear(pred_hidden_dim, pred_hidden_dim),
-            nn.BatchNorm1d(pred_hidden_dim),
-            nn.ReLU(),
             nn.Linear(pred_hidden_dim, proj_output_dim),
         )
 
