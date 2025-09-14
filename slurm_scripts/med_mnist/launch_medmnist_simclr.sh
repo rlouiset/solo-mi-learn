5e147dae652ad5d5b98790e89a6c516b5220d6c3
Robin Louiset <robinlouiset@MacBook-Pro-de-Robin.local>
2025-09-14 19:19:58 +0200

    medmnistdino

--- slurm_scripts/med_mnist/launch_medmnist_simclr.sh
+++ slurm_scripts/med_mnist/launch_medmnist_simclr.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-#SBATCH --job-name simclr_bs_512_bloodmnist
+#SBATCH --job-name dino_bs_512_medmnist
 #SBATCH --time=00-23:59:00
 #SBATCH --nodes=1
 #SBATCH --mem 80G
 #SBATCH --cpus-per-task=4
-#SBATCH --gres=gpu:2
-#SBATCH --gpus-per-node=2
-#SBATCH --partition V100
-#SBATCH --output simclr_bs_512_bloodmnist.txt
+#SBATCH --gres=gpu:1
+#SBATCH --gpus-per-node=1
+#SBATCH --partition A100
+#SBATCH --output dino_bs_512_medmnist.txt
 
 export PATH=/home/ids/rareme/miniconda3/bin:$PATH
 source activate base
-srun python3 main_pretrain.py --config-path scripts/pretrain/med-mnist/ --config-name simclr_blood.yaml
+srun python3 main_pretrain.py --config-path scripts/pretrain/med-mnist/ --config-name dino_blood.yaml
+srun python3 main_pretrain.py --config-path scripts/pretrain/med-mnist/ --config-name dino_path.yaml
