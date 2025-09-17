af51171b69983d050bf1389531e63e2e4c635f2a
Robin Louiset <robinlouiset@MacBook-Pro-de-Robin.local>
2025-09-17 18:52:33 +0200

    in100

--- slurm_scripts/launch_in100_byol.sh
+++ slurm_scripts/launch_in100_byol.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#SBATCH --job-name barlow_bs_256_A100_IN100
+#SBATCH --job-name robyol_A100_IN100
 #SBATCH --time=00-23:59:00
 #SBATCH --nodes=1
 #SBATCH --mem 100G
 #SBATCH --cpus-per-task=8
-#SBATCH --gres=gpu:3
+#SBATCH --gres=gpu:2
 #SBATCH --partition A100
-#SBATCH --output byol_vit_mc_A100_IN100.txt
+#SBATCH --output robyol_A100_IN100.txt
 
 export PATH=/home/ids/rareme/miniconda3/bin:$PATH
 source activate base
 nvidia-smi
-srun python3 main_pretrain.py --config-path scripts/pretrain/imagenet-100-multicrop/ --config-name byol_vit.yaml
+srun python3 main_pretrain.py --config-path scripts/pretrain/imagenet-100/ --config-name robyol.yaml
