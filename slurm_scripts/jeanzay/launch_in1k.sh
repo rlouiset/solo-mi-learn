ebfb1d8883c3a30c45ab4aff780bfebcca5ef456
Robin Louiset <robinlouiset@MacBook-Pro-de-Robin.local>
2025-09-17 20:19:34 +0200

    in1k

--- slurm_scripts/jeanzay/launch_in1k.sh
+++ slurm_scripts/jeanzay/launch_in1k.sh
@@ -3,13 +3,19 @@
 #SBATCH --job-name byol_in1k
 #SBATCH --time=0-19:59:00
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=32               # increase CPU cores for data loading
+# SBATCH --cpus-per-task=64               # increase CPU cores for data loading
 #SBATCH --gres=gpu:4
 #SBATCH --gpus-per-node=4
 #SBATCH --constraint=internet
 #SBATCH --constraint h100
 #SBATCH --account haj@h100
 #SBATCH --output byol_in1k.txt
+
+# Set environment variables for optimal DDP and data loading
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK       # for CPU operations / numpy
+export NCCL_DEBUG=INFO                             # to check DDP communication
+export PYTHONFAULTHANDLER=1
+export MKL_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
 module purge # purge modules inherited by default
 conda deactivate # deactivate environments inherited by default
