b6e4b337f1139b2d74ea212edc5ef33b136c7570
Robin Louiset <robinlouiset@MacBook-Pro-de-Robin.local>
2025-09-17 10:32:16 +0200

    in1k

---
+++ slurm_scripts/jeanzay/launch_in1k.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --job-name byol
+#SBATCH --time=0-23:59:00
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=8
+#SBATCH --gres=gpu:1
+#SBATCH --gpus-per-node=1
+#SBATCH --constraint v100
+#SBATCH --account haj@v100
+#SBATCH --output byol.txt
+
+module purge # purge modules inherited by default
+conda deactivate # deactivate environments inherited by default
+module load miniforge/24.9.0
+conda activate py39
+srun python3 main_pretrain.py --config-path scripts/pretrain/cifar/ --config-name byol.yaml
