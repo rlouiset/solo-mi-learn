75e81fa6cc4cc3583aa900b3d1b21f49b4acce01
Robin Louiset <robinlouiset@mbp-de-robin.home>
2025-09-17 09:37:22 +0200

    medmnist

--- slurm_scripts/med_mnist/launch_oct_mnist_2.sh
+++ slurm_scripts/med_mnist/launch_oct_mnist_2.sh
@@ -13,6 +13,4 @@
 export PATH=/home/ids/rareme/miniconda3/bin:$PATH
 source activate base
 srun python3 main_pretrain.py --config-path scripts/pretrain/med-mnist/ --config-name robyol_oct.yaml
-srun python3 main_pretrain.py --config-path scripts/pretrain/med-mnist/ --config-name simsiam_oct.yaml
-srun python3 main_pretrain.py --config-path scripts/pretrain/med-mnist/ --config-name rosiam_oct.yaml
 
