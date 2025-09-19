d138026642aadcfda3503484813423be9cd055d5
Robin Louiset <robinlouiset@MacBook-Pro-de-Robin.local>
2025-09-19 13:18:41 +0200

    oct

--- slurm_scripts/med_mnist/launch_oct_mnist.sh
+++ slurm_scripts/med_mnist/launch_oct_mnist.sh
@@ -12,6 +12,5 @@
 
 export PATH=/home/ids/rareme/miniconda3/bin:$PATH
 source activate base
-srun python3 main_pretrain.py --config-path scripts/pretrain/med-mnist/ --config-name simclr_oct.yaml
-srun python3 main_pretrain.py --config-path scripts/pretrain/med-mnist/ --config-name dino_oct.yaml
+srun python3 main_pretrain.py --config-path scripts/pretrain/med-mnist/ --config-name byol_oct.yaml
 
