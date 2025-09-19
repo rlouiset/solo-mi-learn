989d3c9e307bea8729e6e928a014548f572a26ef
Robin Louiset <robinlouiset@MacBook-Pro-de-Robin.local>
2025-09-20 01:16:52 +0200

    robyol_2_layers

--- solo/methods/robyol.py
+++ solo/methods/robyol.py
@@ -71,15 +71,6 @@ class RoBYOL(BaseMomentumMethod):
             nn.BatchNorm1d(pred_hidden_dim),
             nn.ReLU(),
             nn.Linear(pred_hidden_dim, pred_hidden_dim),
-            nn.BatchNorm1d(pred_hidden_dim),
-            nn.ReLU(),
-            nn.Linear(pred_hidden_dim, pred_hidden_dim),
-            nn.BatchNorm1d(pred_hidden_dim),
-            nn.ReLU(),
-            nn.Linear(pred_hidden_dim, pred_hidden_dim),
-            nn.BatchNorm1d(pred_hidden_dim),
-            nn.ReLU(),
-            nn.Linear(pred_hidden_dim, proj_output_dim),
         )
 
     @staticmethod
