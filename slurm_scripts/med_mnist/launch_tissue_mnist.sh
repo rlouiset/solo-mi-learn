db7b338a0cb1cde464b9322339ef4b67ee048f79
Robin Louiset <robinlouiset@MacBook-Pro-de-Robin.local>
2025-09-21 13:14:18 +0200

    tissue

--- slurm_scripts/med_mnist/launch_tissue_mnist.sh
+++ slurm_scripts/med_mnist/launch_tissue_mnist.sh
@@ -12,5 +12,5 @@
 
 export PATH=/home/ids/rareme/miniconda3/bin:$PATH
 source activate base
-srun python3 main_pretrain.py --config-path scripts/pretrain/med-mnist/ --config-name mocov2plus_tissue.yaml
+srun python3 main_pretrain.py --config-path scripts/pretrain/med-mnist/ --config-name robyol_tissue.yaml
 
