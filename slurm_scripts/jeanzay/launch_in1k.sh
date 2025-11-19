f177d25908d176a20905bb7d9089c27078a064bc
Robin Louiset <robinlouiset@MacBook-Pro-de-Robin.local>
2025-11-19 11:59:04 +0100

    solo

--- slurm_scripts/jeanzay/launch_in1k.sh
+++ slurm_scripts/jeanzay/launch_in1k.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --job-name byol_in1k
-#SBATCH --time=0-19:59:59
+#SBATCH --time=0-19:59:00
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=32               # increase CPU cores for data loading
 #SBATCH --gres=gpu:2
