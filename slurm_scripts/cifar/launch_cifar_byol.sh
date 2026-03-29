f1b33914beb10bc94bc2cfaadacbd22847e3b72f
Robin Louiset <robinlouiset@mbp-de-robin.home>
2026-03-29 16:11:57 +0200

    assumptions check

--- slurm_scripts/cifar/launch_cifar_byol.sh
+++ slurm_scripts/cifar/launch_cifar_byol.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 #SBATCH --job-name byol
-#SBATCH --time=00-23:59:00
+#SBATCH --time=00-22:59:00
 #SBATCH --nodes=1
 #SBATCH --mem 80G
 #SBATCH --cpus-per-task=4
 #SBATCH --gres=gpu:1
 #SBATCH --gpus-per-node=1
-#SBATCH --partition V100
+#SBATCH --partition gpu-p1
 #SBATCH --output byol.txt
 
-export PATH=/home/ids/rareme/miniconda3/bin:$PATH
-source activate base
+source activate py39
 srun python3 main_pretrain.py --config-path scripts/pretrain/cifar/ --config-name byol.yaml
